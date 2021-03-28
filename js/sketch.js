3085c96184788108bca33706e1f0ecc5a638efac
lilcode-dev <wilfrigm02@gmail.com>
2021-03-28 08:03:28 +0800

    work in ColorThiet

--- js/sketch.js
+++ js/sketch.js
@@ -1,13 +1,14 @@
+const getImageData = () => document.querySelector('.img-predict');
 const mobilenetReady = () => {
     console.log('mobilenet is ready!');
 }
+const colorThief = new ColorThief();
 let mobilenet = ml5.imageClassifier('MobileNet', mobilenetReady());
 
 const fileReceived = (fileName) => {
     console.log(`file name: ${fileName}`);
     predictImage();
 }
-const getImageData = () => document.querySelector('.img-predict');
 
 const predictImage = () => {
     mobilenet.predict( getImageData(), (err, results) => {
